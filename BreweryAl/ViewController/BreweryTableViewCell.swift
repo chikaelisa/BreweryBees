cfcb4b7b4e00a3756765a3f06c6bf1dcc74c96b9
Francisca Rosa <f.rosa96@hotmail.com>
2022-02-28 15:29:14 -0300

    layout cell with constraints available

---
+++ BreweryAl/ViewController/BreweryTableViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  BreweryTableViewCell.swift
+//  BreweryAl
+//
+//  Created by Francisca Elisa Carvalho Rosa on 28/02/22.
+//
+
+import UIKit
+
+class BreweryTableViewCell: UITableViewCell {
+
+    static let identifier = "BreweryTableViewCell"
+    
+    let model = ["Ademir", "Vila Bar", "Stout", "Cabaré", "As mina", "10 Shots"]
+    
+    @IBOutlet weak var imageBrew: UIImageView!
+    @IBOutlet weak var nameBrew: UILabel!
+    @IBOutlet weak var rateBrew: UILabel!
+    @IBOutlet weak var typeBrew: UILabel!
+    @IBOutlet weak var distanceBrew: UILabel!
+    @IBOutlet weak var isFavoriteButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageBrew.layer.cornerRadius = imageBrew.frame.height / 2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func shareAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func isFavoriteAction(_ sender: Any) {
+    }
+    
+    func configureCell(breweModel: String) {
+        nameBrew.text = breweModel
+        if var firstChar = breweModel.first{
+            firstChar = Character(firstChar.lowercased())
+            imageBrew.image = UIImage(systemName: "\(firstChar).circle")
+        }
+    }
+    
+    
+}
