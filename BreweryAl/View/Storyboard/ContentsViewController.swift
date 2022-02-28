7e854ef78eb7918344fcd696b4d611700b51b9a4
Francisca Rosa <f.rosa96@hotmail.com>
2022-02-28 11:50:02 -0300

    reference Storyboard and contensContainer available

---
+++ BreweryAl/View/Storyboard/ContentsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContentsViewController.swift
+//  BreweryAl
+//
+//  Created by Francisca Elisa Carvalho Rosa on 28/02/22.
+//
+
+import UIKit
+
+class ContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
